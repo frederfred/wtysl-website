c11c98db21d18b76b05d2adedbc4ad844f197db2
Fred Bergman <fred@fbergman.se>
2015-05-29 12:20:14 +0200

    Add content to footer

---
+++ web/app/themes/wtysl/partials/newsletter_form.php
@@ -0,0 +1,4 @@
+<form action="/" method="post">
+  <label for="<?php echo $data->id; ?>-newsletter-email">E-mail</label>
+  <input type="email" id="<?php echo $data->id; ?>-newsletter-email">
+</form>
