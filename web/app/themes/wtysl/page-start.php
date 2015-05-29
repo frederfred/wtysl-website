928d7569896308cd6ae517df679bb6ec3ba12483
Fred Bergman <fred@fbergman.se>
2015-05-29 15:58:00 +0200

    Indent

--- web/app/themes/wtysl/page-start.php
+++ web/app/themes/wtysl/page-start.php
@@ -2,31 +2,32 @@
   <?php if (have_posts()): ?>
     <?php while (have_posts()): ?>
       <?php the_post(); ?>
-        <div class="Wrapper Wrapper--bleed">
-          <div class="u-trailer-l">
-            start/media_intro
-          </div>
+
+      <div class="Wrapper Wrapper--bleed">
+        <div class="u-trailer-l">
+          start/media_intro
+        </div>
+      </div>
+
+      <div class="Wrapper">
+        <div class="u-trailer-l">
+          start/projects
         </div>
 
-        <div class="Wrapper">
-          <div class="u-trailer-l">
-            start/projects
-          </div>
-
-          <div class="u-trailer-l">
-            start/who_we_are
-          </div>
-
-          <div class="u-trailer-l">
-            start/how_we_work
-          </div>
-
-          <div class="u-trailer-l">
-            <h2>Latest from our Instagram feed</h2>
-
-            <?php echo get_instagram_feed(); ?>
-          </div>
+        <div class="u-trailer-l">
+          start/who_we_are
         </div>
+
+        <div class="u-trailer-l">
+          start/how_we_work
+        </div>
+
+        <div class="u-trailer-l">
+          <h2>Latest from our Instagram feed</h2>
+
+          <?php echo get_instagram_feed(); ?>
+        </div>
+      </div>
     <?php endwhile; ?>
   <?php endif; ?>
 <?php get_footer(); ?>
