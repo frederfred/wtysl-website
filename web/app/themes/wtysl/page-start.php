096863f8c6ad7b06ae535e80881e2ed946624999
Fred Bergman <fred@fbergman.se>
2015-10-27 21:10:14 +0100

    Remove unwanted sections from startpage

--- web/app/themes/wtysl/page-start.php
+++ web/app/themes/wtysl/page-start.php
@@ -1,5 +1,3 @@
-<?php $projects = get_archive_posts("projects", 4); ?>
-
 <?php get_header(); ?>
 
 <div class="Wrapper Wrapper--bleed">
@@ -9,40 +7,6 @@
 </div>
 
 <div class="Wrapper">
-  <div class="u-trailer-l">
-    <h2>
-      Selected projects
-
-      <div class="Meta u-displayInlineBlock">
-        <a href="<?php echo get_post_type_archive_link("projects"); ?>">See all</a>
-      </div>
-    </h2>
-
-    <div class="Grid Grid--12-6-3">
-      <?php while ($projects->have_posts()): ?>
-        <?php $projects->the_post(); ?>
-
-        <a href="<?php the_permalink(); ?>">
-          <div class="Grid-item">
-            <div class="FlexEmbed FlexEmbed--16by9">
-              <img src="https://placehold.it/660x371?todo" class="FlexEmbed-item" alt="Project">
-            </div>
-
-            <h3><?php the_title(); ?></h3>
-          </div>
-        </a>
-      <?php endwhile; ?>
-    </div>
-  </div>
-
-  <div class="u-trailer-l">
-    start/who_we_are
-  </div>
-
-  <div class="u-trailer-l">
-    start/how_we_work
-  </div>
-
   <div class="u-trailer-l">
     <h2>Latest from our Instagram feed</h2>
 
