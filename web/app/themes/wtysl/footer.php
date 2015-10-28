71ade90b56dbd9d7353519ee954fcdc7cf3f41ea
Fred Bergman <fred@fbergman.se>
2015-10-28 21:36:29 +0100

    Remove footer

--- web/app/themes/wtysl/footer.php
+++ web/app/themes/wtysl/footer.php
@@ -1,26 +1,5 @@
   </main>
 
-  <footer class="Footer" role="contentinfo">
-    <div class="Wrapper">
-      <div class="Footer-inner">
-        <ul class="Footer-contact">
-          <li class="Footer-contactItem"><a href="mailto:info@whattookyousolong.org">info@whattookyousolong.org</a>
-          <li class="Footer-contactItem"><a href="https://www.facebook.com/WhatTookYouSoLong" target="_blank">Like us on Facebook</a>
-          <li class="Footer-contactItem"><a href="https://twitter.com/wtysl" target="_blank">Follow us on Twitter</a>
-          <li class="Footer-contactItem"><a href="https://instagram.com/wtysl/" target="_blank">Follow us on Instagram</a>
-        </ul>
-
-        <div class="Footer-newsletter">
-          <h3>Subscribe to our newsletter</h3>
-
-          <p>Text about why the newsletter is a good thing to recive.</p>
-
-          <?php echo include_partial("partials/newsletter_form", array("id" => "footer")); ?>
-        </div>
-      </div>
-    </div>
-  </footer>
-
   <?php if (getenv("WP_ENV") === "production"): ?>
     <script>
       var _gaq=[['_setAccount','UA-XXXXXXXX-1'],['_trackPageview']];
