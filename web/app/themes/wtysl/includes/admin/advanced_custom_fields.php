82362c48ab5b242db05952ebfd3796f6e2c85bc5
Fred Bergman <fred@fbergman.se>
2015-10-31 12:59:03 +0100

    Add vimeo id custom field to startpage

--- web/app/themes/wtysl/includes/admin/advanced_custom_fields.php
+++ web/app/themes/wtysl/includes/admin/advanced_custom_fields.php
@@ -74,4 +74,41 @@ if(function_exists("register_field_group"))
     ),
     'menu_order' => 0,
   ));
+
+  register_field_group(array (
+    'id' => 'acf_startpage',
+    'title' => 'Startpage',
+    'fields' => array (
+      array (
+        'key' => 'field_56313808216f8',
+        'label' => 'Vimeo id',
+        'name' => 'vimeo_id',
+        'type' => 'text',
+        'default_value' => '',
+        'placeholder' => '',
+        'prepend' => '',
+        'append' => '',
+        'formatting' => 'none',
+        'maxlength' => '',
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'page',
+          'operator' => '==',
+          'value' => '6',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'normal',
+      'layout' => 'no_box',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => 0,
+  ));
 }
