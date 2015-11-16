bc6c9004fb544a5b400248cd098dd4f90f794fdd
Fred Bergman <fred@fbergman.se>
2015-11-16 23:12:44 +0100

    Add single project template

---
+++ web/app/themes/wtysl/single-project.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()): ?>
+  <?php while (have_posts()): ?>
+    <?php the_post(); ?>
+      <div class="Wrapper">
+        <article>
+          <div class="FlexEmbed FlexEmbed--16by9 u-trailer-m">
+            <iframe src="https://player.vimeo.com/video/95968697?badge=0&amp;byline=0&amp;portrait=0&amp;title=0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="FlexEmbed-item"></iframe>
+          </div>
+
+          <h1><?php the_title(); ?></h1>
+
+          <?php the_content(); ?>
+        </article>
+      </div>
+  <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
