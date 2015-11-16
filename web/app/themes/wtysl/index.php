f8a0daf56c25ee5f4ef33db335caa5a46488378c
Fred Bergman <fred@fbergman.se>
2015-11-16 23:12:34 +0100

    Cleanup

--- web/app/themes/wtysl/index.php
+++ web/app/themes/wtysl/index.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
-  <?php if (have_posts()): ?>
-    <?php while (have_posts()): ?>
-      <?php the_post(); ?>
-        <div class="Wrapper">
-          <article>
-            <h1><?php the_title(); ?></h1>
 
-            <?php the_content(); ?>
-          </article>
-        </div>
-    <?php endwhile; ?>
-  <?php endif; ?>
+<?php if (have_posts()): ?>
+  <?php while (have_posts()): ?>
+    <?php the_post(); ?>
+      <div class="Wrapper">
+        <article>
+          <h1><?php the_title(); ?></h1>
+
+          <?php the_content(); ?>
+        </article>
+      </div>
+  <?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
