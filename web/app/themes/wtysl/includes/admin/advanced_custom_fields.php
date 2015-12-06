20f07fc8e03508d95808ff7b77874fe9e7570265
Fred Bergman <fred@fbergman.se>
2015-12-06 11:18:40 +0100

    Update custom fields

--- web/app/themes/wtysl/includes/admin/advanced_custom_fields.php
+++ web/app/themes/wtysl/includes/admin/advanced_custom_fields.php
@@ -3,87 +3,14 @@
 if(function_exists("register_field_group"))
 {
   register_field_group(array (
-    'id' => 'acf_project-meta',
-    'title' => 'Project meta',
-    'fields' => array (
-      array (
-        'key' => 'field_557aca0da6f4a',
-        'label' => 'Year',
-        'name' => 'year',
-        'type' => 'number',
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
-        'append' => '',
-        'min' => '',
-        'max' => '',
-        'step' => '',
-      ),
-      array (
-        'key' => 'field_557aca75a6f4b',
-        'label' => 'Location',
-        'name' => 'location',
-        'type' => 'text',
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
-        'append' => '',
-        'formatting' => 'html',
-        'maxlength' => '',
-      ),
-      array (
-        'key' => 'field_557acaa0a6f4c',
-        'label' => 'Client',
-        'name' => 'client',
-        'type' => 'text',
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
-        'append' => '',
-        'formatting' => 'html',
-        'maxlength' => '',
-      ),
-      array (
-        'key' => 'field_557acad9a6f4d',
-        'label' => 'Team',
-        'name' => 'team',
-        'type' => 'textarea',
-        'default_value' => '',
-        'placeholder' => '',
-        'maxlength' => '',
-        'rows' => '',
-        'formatting' => 'none',
-      ),
-    ),
-    'location' => array (
-      array (
-        array (
-          'param' => 'post_type',
-          'operator' => '==',
-          'value' => 'projects',
-          'order_no' => 0,
-          'group_no' => 0,
-        ),
-      ),
-    ),
-    'options' => array (
-      'position' => 'normal',
-      'layout' => 'default',
-      'hide_on_screen' => array (
-      ),
-    ),
-    'menu_order' => 0,
-  ));
-
-  register_field_group(array (
     'id' => 'acf_startpage',
-    'title' => 'Startpage',
+    'title' => 'Startpage meta',
     'fields' => array (
       array (
         'key' => 'field_56313808216f8',
         'label' => 'Vimeo id',
         'name' => 'vimeo_id',
-        'type' => 'text',
+        'type' => 'number',
         'default_value' => '',
         'placeholder' => '',
         'prepend' => '',
@@ -105,7 +32,45 @@ if(function_exists("register_field_group"))
     ),
     'options' => array (
       'position' => 'normal',
-      'layout' => 'no_box',
+      'layout' => 'default',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => 0,
+  ));
+
+  register_field_group(array (
+    'id' => 'acf_project-meta',
+    'title' => 'Project meta',
+    'fields' => array (
+      array (
+        'key' => 'field_56640943d32d0',
+        'label' => 'Vimeo ID',
+        'name' => 'vimeo_id',
+        'type' => 'number',
+        'default_value' => '',
+        'placeholder' => '',
+        'prepend' => '',
+        'append' => '',
+        'min' => '',
+        'max' => '',
+        'step' => '',
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'project',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'normal',
+      'layout' => 'default',
       'hide_on_screen' => array (
       ),
     ),
