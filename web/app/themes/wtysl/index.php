e6c88780ae9d6c7e8c1badd351fb055e541a99f2
Fred Bergman <fred@fbergman.se>
2015-12-06 12:18:28 +0100

    Begin work on text page

--- web/app/themes/wtysl/index.php
+++ web/app/themes/wtysl/index.php
@@ -4,10 +4,14 @@
   <?php while (have_posts()): ?>
     <?php the_post(); ?>
       <div class="Wrapper">
-        <article>
-          <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
+        <article class="Grid Grid--center-12-8">
+          <div class="Grid-item">
+            <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
 
-          <?php the_content(); ?>
+            <div class="Text">
+              <?php the_content(); ?>
+            </div>
+          </div>
         </article>
       </div>
   <?php endwhile; ?>
