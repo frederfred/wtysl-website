edd59480ec625277520eff86fa74cc09b1080967
Fred Bergman <fred@fbergman.se>
2015-12-06 11:28:39 +0100

    Add dynamic video to project page

--- web/app/themes/wtysl/single-project.php
+++ web/app/themes/wtysl/single-project.php
@@ -3,13 +3,19 @@
 <?php if (have_posts()): ?>
   <?php while (have_posts()): ?>
     <?php the_post(); ?>
+      <?php
+      $video_id = get_post_meta(get_the_ID(), 'vimeo_id', true);
+      ?>
+
       <div class="Wrapper">
         <article>
           <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
 
-          <div class="FlexEmbed FlexEmbed--16by9 u-trailer-m">
-            <iframe src="https://player.vimeo.com/video/95968697?badge=0&amp;byline=0&amp;portrait=0&amp;title=0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="FlexEmbed-item"></iframe>
-          </div>
+          <?php if ($video_id): ?>
+            <div class="FlexEmbed FlexEmbed--16by9 u-trailer-m">
+              <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>?badge=0&amp;byline=0&amp;portrait=0&amp;title=0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="FlexEmbed-item"></iframe>
+            </div>
+          <?php endif; ?>
 
           <?php the_content(); ?>
         </article>
