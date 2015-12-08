99c93c7bcb34ba8a2fc37e49e605a040f0d07741
Fred Bergman <fred@fbergman.se>
2015-12-08 08:06:42 +0100

    Update project grid text

--- web/app/themes/wtysl/templates/projects.php
+++ web/app/themes/wtysl/templates/projects.php
@@ -13,16 +13,24 @@ $query = new WP_Query(
       <?php while ($query->have_posts()): ?>
         <?php $query->the_post(); ?>
         <?php $image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'small')[0]; ?>
+        <?php $categories = get_the_category(); ?>
 
         <li class="Grid-item">
           <a href="<?php the_permalink(); ?>" class="ProjectItem">
             <?php if ($image_src): ?>
-              <div class="FlexEmbed FlexEmbed--6by4 u-trailer-xs ProjectItem-media">
+              <div class="FlexEmbed FlexEmbed--16by9 ProjectItem-media">
                 <img src="<?php echo $image_src; ?>" alt="<?php the_title(); ?>" class="FlexEmbed-item">
               </div>
             <?php endif; ?>
 
-            <h2 class="Headline Headline--3"><?php the_title(); ?></h2>
+            <div class="ProjectItem-text">
+              <h2 class="Headline Headline--3"><?php the_title(); ?></h2>
+              <?php
+              foreach ($categories as $category) {
+                echo $category->name;
+              }
+              ?>
+            </div>
           </a>
         </li>
       <?php endwhile; ?>
