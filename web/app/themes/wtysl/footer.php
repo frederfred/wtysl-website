4775c1119ed3f00a14f1a1f98e9358d146a5e592
Fred Bergman <fred@fbergman.se>
2015-12-23 18:45:56 +0100

    Add footer

--- web/app/themes/wtysl/footer.php
+++ web/app/themes/wtysl/footer.php
@@ -1,5 +1,11 @@
   </main>
 
+  <div class="Wrapper">
+    <footer class="Footer">
+      <img src="<?php bloginfo("template_url"); ?>/public/assets/images/logotype-minimal.svg" alt='"What Took You So Long?" minimal logotype' class="Footer-logotype">
+    </footer>
+  </div>
+
   <?php if (getenv("WP_ENV") === "production"): ?>
     <script>
       var _gaq=[['_setAccount','UA-XXXXXXXX-1'],['_trackPageview']];
