15311aacf66d02f92532466c115ee5addab0b330
Fred Bergman <fred@fbergman.se>
2015-12-30 15:59:53 +0100

    Cleanup

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,6 +2,6 @@ var gulp = require("gulp");
 var del = require("del");
 var config = require("../config");
 
-gulp.task("clean", function (cb) {
+gulp.task("clean", function(cb) {
   del([config.publicAssets], cb);
 });
