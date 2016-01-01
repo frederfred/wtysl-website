17a4714a2c1fc6c7e4b2e0b723c47fcaa8be72cd
Fred Bergman <fred@fbergman.se>
2016-01-01 23:32:24 +0100

    Allow deploy to multiple environments

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,19 +1,30 @@
 var rsync = require("rsyncwrapper").rsync;
 var gulp = require("gulp");
 var gutil = require("gulp-util");
+var yargs = require("yargs").argv;
 var config = require("../config").deploy;
-var target = require("../../deploy-settings.json").staging;
-var rsyncConfig = Object.assign({
-  dest: target.user + "@" + target.host + ":" + target.destination
-}, config.rsync);
+var allowedTargets = ["staging", "production"];
 
 gulp.task("deploy", function() {
-  rsync(rsyncConfig, function(error) {
-    if (error) {
-      gutil.log(gutil.colors.red("Deploy error"));
-      gutil.log(error.message);
-    } else {
-      gutil.log(gutil.colors.green("Deploy complete"));
-    }
-  });
+  var target = yargs.env;
+  var settings;
+  var rsyncConfig;
+
+  if (allowedTargets.indexOf(target) > -1) {
+    settings = require("../../deploy-settings.json")[target];
+    rsyncConfig = Object.assign({
+      dest: settings.user + "@" + settings.host + ":" + settings.destination
+    }, config.rsync);
+
+    rsync(rsyncConfig, function(error) {
+      if (error) {
+        gutil.log(gutil.colors.red("Deploy error"));
+        gutil.log(error.message);
+      } else {
+        gutil.log(gutil.colors.green("Deploy complete"));
+      }
+    });
+  } else {
+    gutil.log(gutil.colors.red("Error.") + " --env must be either " + allowedTargets.join(" or "));
+  }
 });
