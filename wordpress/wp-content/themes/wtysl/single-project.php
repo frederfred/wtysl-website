6ead422bdaae161c2297f3248fdc01930e52fa1a
Fred Bergman <fred@fbergman.se>
2016-01-05 15:42:30 +0100

    Move headline

--- wordpress/wp-content/themes/wtysl/single-project.php
+++ wordpress/wp-content/themes/wtysl/single-project.php
@@ -9,8 +9,6 @@
 
       <div class="Wrapper u-trailer-xl">
         <article>
-          <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
-
           <?php if ($video_id): ?>
             <div class="FlexEmbed FlexEmbed--16by9 u-trailer-xl">
               <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>?badge=0&amp;byline=0&amp;portrait=0&amp;title=0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="FlexEmbed-item"></iframe>
@@ -19,6 +17,8 @@
 
           <article class="Grid Grid--center-12-8">
             <div class="Grid-item">
+              <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
+
               <div class="Text">
                 <?php the_content(); ?>
               </div>
