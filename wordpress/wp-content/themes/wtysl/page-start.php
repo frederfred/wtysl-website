d4772a1bf8825ab62e00343855224fcde9e88fd4
Fred Bergman <fred@fbergman.se>
2016-01-05 17:19:11 +0100

    Add featured projects to startpage

--- wordpress/wp-content/themes/wtysl/page-start.php
+++ wordpress/wp-content/themes/wtysl/page-start.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 
+<?php
+$featured_projects = new WP_Query(
+  array(
+    'post_type' => 'project',
+    'posts_per_page' => 2
+  )
+);
+?>
+
 <div class="MediaIntro">
   <div class="MediaIntro-imageWrapper">
     <div class="MediaIntro-image" style="background-image: url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')[0]; ?>);"></div>
@@ -25,6 +34,40 @@
 </div>
 
 <div class="Wrapper">
+  <?php if ($featured_projects->have_posts()): ?>
+    <h2 class="Headline Headline--2 u-textCenter">Selected projects</h2>
+
+    <ul class="Grid Grid--12-6 u-listPlain u-trailer-xl">
+      <?php while ($featured_projects->have_posts()): ?>
+        <?php $featured_projects->the_post(); ?>
+        <?php $image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'small')[0]; ?>
+        <?php $categories = get_the_category(); ?>
+
+        <li class="Grid-item">
+          <a href="<?php the_permalink(); ?>" class="ProjectItem">
+            <?php if ($image_src): ?>
+              <div class="FlexEmbed FlexEmbed--16by9 ProjectItem-media">
+                <img src="<?php echo $image_src; ?>" alt="<?php the_title(); ?>" class="FlexEmbed-item">
+              </div>
+            <?php endif; ?>
+
+            <div class="ProjectItem-text">
+              <h2 class="ProjectItem-title Headline Headline--3"><?php the_title(); ?></h2>
+
+              <?php if (!empty($categories)): ?>
+                <p class="ProjectItem-category">
+                  <?php echo $categories[0]->name; ?>
+                </p>
+              <?php endif; ?>
+            </div>
+          </a>
+        </li>
+      <?php endwhile; ?>
+    </ul>
+  <?php endif; ?>
+
+  <?php wp_reset_postdata(); ?>
+
   <?php echo get_instagram_feed(); ?>
 </div>
 
