ae9dc5a0cef45fa4514092ff984eddf6bb5e2d1d
Fred Bergman <fred@fbergman.se>
2016-01-25 23:22:23 +0100

    Fix indent

--- wordpress/wp-content/themes/wtysl/single-project.php
+++ wordpress/wp-content/themes/wtysl/single-project.php
@@ -4,52 +4,52 @@
   <?php while (have_posts()): ?>
     <?php the_post(); ?>
 
-      <div class="Wrapper u-trailer-xl">
-        <article>
-          <?php if (get_field('vimeo_id')): ?>
-            <div class="FlexEmbed FlexEmbed--16by9 u-trailer-xl">
-              <iframe src="https://player.vimeo.com/video/<?php the_field('vimeo_id'); ?>?badge=0&amp;byline=0&amp;portrait=0&amp;title=0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="FlexEmbed-item"></iframe>
-            </div>
-          <?php endif; ?>
+    <div class="Wrapper u-trailer-xl">
+      <article>
+        <?php if (get_field('vimeo_id')): ?>
+          <div class="FlexEmbed FlexEmbed--16by9 u-trailer-xl">
+            <iframe src="https://player.vimeo.com/video/<?php the_field('vimeo_id'); ?>?badge=0&amp;byline=0&amp;portrait=0&amp;title=0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="FlexEmbed-item"></iframe>
+          </div>
+        <?php endif; ?>
 
-          <div class="Grid Grid--center-12-8">
-            <div class="Grid-item">
-              <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
+        <div class="Grid Grid--center-12-8">
+          <div class="Grid-item">
+            <h1 class="Headline Headline--1"><?php the_title(); ?></h1>
 
-              <div class="Text u-trailer-l">
-                <?php the_content(); ?>
-              </div>
+            <div class="Text u-trailer-l">
+              <?php the_content(); ?>
             </div>
           </div>
+        </div>
 
-          <?php if (have_rows('images')): ?>
-            <?php if (has_odd_number_of_images()): ?>
-              <?php $image = get_field('images')[0]['image']; ?>
+        <?php if (have_rows('images')): ?>
+          <?php if (has_odd_number_of_images()): ?>
+            <?php $image = get_field('images')[0]['image']; ?>
 
-              <div class="Grid Grid--12">
-                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" class="Grid-item u-trailer-xs">
-              </div>
-            <?php endif; ?>
-
-            <div class="Grid Grid--12-6">
-              <?php $i = 0; ?>
-
-              <?php while (have_rows('images')): ?>
-                <?php the_row(); ?>
-                <?php $image = get_sub_field('image'); ?>
-
-                <?php if (has_odd_number_of_images() && $i != 0): ?>
-                  <div class="Grid-item u-trailer-xs">
-                    <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>">
-                  </div>
-                <?php endif; ?>
-
-                <?php $i++; ?>
-              <?php endwhile; ?>
+            <div class="Grid Grid--12">
+              <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" class="Grid-item u-trailer-xs">
             </div>
           <?php endif; ?>
-        </article>
-      </div>
+
+          <div class="Grid Grid--12-6">
+            <?php $i = 0; ?>
+
+            <?php while (have_rows('images')): ?>
+              <?php the_row(); ?>
+              <?php $image = get_sub_field('image'); ?>
+
+              <?php if (has_odd_number_of_images() && $i != 0): ?>
+                <div class="Grid-item u-trailer-xs">
+                  <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>">
+                </div>
+              <?php endif; ?>
+
+              <?php $i++; ?>
+            <?php endwhile; ?>
+          </div>
+        <?php endif; ?>
+      </article>
+    </div>
   <?php endwhile; ?>
 <?php endif; ?>
 
