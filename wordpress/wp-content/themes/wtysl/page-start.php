38cb0a4dfcbb6a31a289944cef834db9e3dcf784
Fred Bergman <fred@fbergman.se>
2016-01-25 22:55:44 +0100

    Add link to all projects

--- wordpress/wp-content/themes/wtysl/page-start.php
+++ wordpress/wp-content/themes/wtysl/page-start.php
@@ -34,35 +34,41 @@ $featured_projects = new WP_Query(
 
 <div class="Wrapper">
   <?php if ($featured_projects->have_posts()): ?>
-    <h2 class="Headline Headline--2 u-textCenter">Projects</h2>
+    <div class="u-trailer-xl">
+      <h2 class="Headline Headline--2 u-textCenter">Projects</h2>
 
-    <ul class="Grid Grid--12-4 u-listPlain u-trailer-xl">
-      <?php while ($featured_projects->have_posts()): ?>
-        <?php $featured_projects->the_post(); ?>
-        <?php $image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'small')[0]; ?>
-        <?php $categories = get_the_category(); ?>
+      <ul class="Grid Grid--12-4 u-listPlain">
+        <?php while ($featured_projects->have_posts()): ?>
+          <?php $featured_projects->the_post(); ?>
+          <?php $image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'small')[0]; ?>
+          <?php $categories = get_the_category(); ?>
 
-        <li class="Grid-item">
-          <a href="<?php the_permalink(); ?>" class="ProjectItem">
-            <?php if ($image_src): ?>
-              <div class="FlexEmbed FlexEmbed--16by9 ProjectItem-media">
-                <img src="<?php echo $image_src; ?>" alt="<?php the_title(); ?>" class="FlexEmbed-item">
-              </div>
-            <?php endif; ?>
-
-            <div class="ProjectItem-text">
-              <h2 class="ProjectItem-title Headline Headline--3"><?php the_title(); ?></h2>
-
-              <?php if (!empty($categories)): ?>
-                <p class="ProjectItem-category">
-                  <?php echo $categories[0]->name; ?>
-                </p>
+          <li class="Grid-item">
+            <a href="<?php the_permalink(); ?>" class="ProjectItem">
+              <?php if ($image_src): ?>
+                <div class="FlexEmbed FlexEmbed--16by9 ProjectItem-media">
+                  <img src="<?php echo $image_src; ?>" alt="<?php the_title(); ?>" class="FlexEmbed-item">
+                </div>
               <?php endif; ?>
-            </div>
-          </a>
-        </li>
-      <?php endwhile; ?>
-    </ul>
+
+              <div class="ProjectItem-text">
+                <h2 class="ProjectItem-title Headline Headline--3"><?php the_title(); ?></h2>
+
+                <?php if (!empty($categories)): ?>
+                  <p class="ProjectItem-category">
+                    <?php echo $categories[0]->name; ?>
+                  </p>
+                <?php endif; ?>
+              </div>
+            </a>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+
+      <div class="u-textCenter Meta">
+        <a href="<?php echo get_post_type_archive_link('project'); ?>">See all projects</a>
+      </div>
+    </div>
   <?php endif; ?>
 
   <?php wp_reset_postdata(); ?>
