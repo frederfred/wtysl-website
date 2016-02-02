805dd037ba77002af9a8622b8a062ff305fb67ab
Fred Bergman <fred@fbergman.se>
2016-02-02 21:00:12 +0100

    Fix Google Analytics tracking

--- wordpress/wp-content/themes/wtysl/footer.php
+++ wordpress/wp-content/themes/wtysl/footer.php
@@ -10,12 +10,15 @@
     </footer>
   </div>
 
-  <?php if (getenv("WP_ENV") === "production"): ?>
+  <?php if (defined('ENV_GOOGLE_ANALYTICS_ID')): ?>
     <script>
-      var _gaq=[['_setAccount','UA-XXXXXXXX-1'],['_trackPageview']];
-      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-      s.parentNode.insertBefore(g,s)}(document,'script'));
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '<?php echo ENV_GOOGLE_ANALYTICS_ID; ?>', 'auto');
+      ga('send', 'pageview');
     </script>
   <?php endif; ?>
 
