5bbfc173e0e24460d6063ce7ec8ebbf9b00c195a
Fred Bergman <fred@fbergman.se>
2016-02-02 09:57:34 +0100

    Fix indent

--- wordpress/wp-content/themes/wtysl/index.php
+++ wordpress/wp-content/themes/wtysl/index.php
@@ -3,24 +3,25 @@
 <?php if (have_posts()): ?>
   <?php while (have_posts()): ?>
     <?php the_post(); ?>
-      <?php $featured_image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')[0]; ?>
-      <?php $headline_color = dashes_to_camel_case(get_field('headline_color')); ?>
 
-      <?php if ($featured_image_src): ?>
-        <div class="FeaturedImage" style="background-image: url(<?php echo $featured_image_src; ?>);">
-          <h1 class="FeaturedImage-headline FeaturedImage-headline--<?php echo $headline_color; ?> Headline Headline--1"><?php the_title(); ?></h1>
-        </div>
-      <?php endif; ?>
+    <?php $featured_image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')[0]; ?>
+    <?php $headline_color = dashes_to_camel_case(get_field('headline_color')); ?>
 
-      <div class="Wrapper">
-        <article class="Grid Grid--center-12-8">
-          <div class="Grid-item">
-            <div class="Text">
-              <?php the_content(); ?>
-            </div>
-          </div>
-        </article>
+    <?php if ($featured_image_src): ?>
+      <div class="FeaturedImage" style="background-image: url(<?php echo $featured_image_src; ?>);">
+        <h1 class="FeaturedImage-headline FeaturedImage-headline--<?php echo $headline_color; ?> Headline Headline--1"><?php the_title(); ?></h1>
       </div>
+    <?php endif; ?>
+
+    <div class="Wrapper">
+      <article class="Grid Grid--center-12-8">
+        <div class="Grid-item">
+          <div class="Text">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </article>
+    </div>
   <?php endwhile; ?>
 <?php endif; ?>
 
