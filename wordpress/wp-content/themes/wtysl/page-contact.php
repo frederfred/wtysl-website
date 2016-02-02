ae37f4131796b7972ab22eb7ac1485fe030e5d6d
Fred Bergman <fred@fbergman.se>
2016-02-02 09:32:29 +0100

    Fix indent

--- wordpress/wp-content/themes/wtysl/page-contact.php
+++ wordpress/wp-content/themes/wtysl/page-contact.php
@@ -3,19 +3,20 @@
 <?php if (have_posts()): ?>
   <?php while (have_posts()): ?>
     <?php the_post(); ?>
-      <div class="Wrapper u-trailer-xl">
-        <article class="Grid Grid--center-12-8">
-          <div class="Grid-item">
-            <h1 class="Headline Headline--1 u-visuallyhidden"><?php the_title(); ?></h1>
 
-            <div class="Text u-trailer-xl">
-              <?php the_content(); ?>
-            </div>
+    <div class="Wrapper u-trailer-xl">
+      <article class="Grid Grid--center-12-8">
+        <div class="Grid-item">
+          <h1 class="Headline Headline--1 u-visuallyhidden"><?php the_title(); ?></h1>
 
-            <?php get_template_part('templates/newsletter_form'); ?>
+          <div class="Text u-trailer-xl">
+            <?php the_content(); ?>
           </div>
-        </article>
-      </div>
+
+          <?php get_template_part('templates/newsletter_form'); ?>
+        </div>
+      </article>
+    </div>
   <?php endwhile; ?>
 <?php endif; ?>
 
