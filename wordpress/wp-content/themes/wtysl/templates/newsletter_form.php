55ebf974ca265edeb77113866bb3ff0d42828078
Fred Bergman <fred@fbergman.se>
2016-02-02 09:22:29 +0100

    Add dynamic Mailchimp details

--- wordpress/wp-content/themes/wtysl/templates/newsletter_form.php
+++ wordpress/wp-content/themes/wtysl/templates/newsletter_form.php
@@ -1,4 +1,4 @@
-<form class="Newsletter" action="//todo.us9.list-manage.com/subscribe/post?u=todo&amp;id=todo" method="post" target="_blank">
+<form class="Newsletter" action="//whattookyousolong.<?php echo MAILCHIMP_DATACENTER; ?>.list-manage.com/subscribe/post?u=<?php echo MAILCHIMP_USER; ?>&amp;id=<?php echo MAILCHIMP_ID; ?>" method="post" target="_blank" novalidate>
   <p>Subscribe to our newsletter</p>
 
   <label class="Newsletter-label" for="newsletter-email">Email:</label>
@@ -7,7 +7,7 @@
 
   <?php # real people should not fill this in and expect good things - do not remove this or risk form bot signups ?>
   <div class="u-visuallyhidden">
-    <input class="u-visuallyhidden" type="text" name="todo" tabindex="-1" value="">
+    <input class="u-visuallyhidden" type="text" name="b_<?php echo MAILCHIMP_USER; ?>_<?php echo MAILCHIMP_ID; ?>" tabindex="-1" value="">
   </div>
 
   <input class="Newsletter-submit" type="submit" value="Subscribe" name="subscribe">
