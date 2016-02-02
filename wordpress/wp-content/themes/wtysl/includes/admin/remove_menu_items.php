854ebc1d3a22a4e53a0216fa1537740d6cd4af2e
Fred Bergman <fred@fbergman.se>
2016-02-02 11:07:55 +0100

    Remove posts from WordPress admin menu

--- wordpress/wp-content/themes/wtysl/includes/admin/remove_menu_items.php
+++ wordpress/wp-content/themes/wtysl/includes/admin/remove_menu_items.php
@@ -3,5 +3,6 @@
 add_action('admin_menu', 'remove_admin_menus');
 
 function remove_admin_menus() {
-  remove_menu_page('edit-comments.php');
+  remove_menu_page('edit.php'); # Posts
+  remove_menu_page('edit-comments.php'); # Comments
 }
