62f7f6889748c396a9ca1bedf79c62563a693df8
Fred Bergman <fred@fbergman.se>
2016-02-02 09:16:46 +0100

    Remove unused file

--- wordpress/wp-content/themes/wtysl/partials/newsletter_form.php
+++
@@ -1,4 +0,0 @@
-<form action="/" method="post">
-  <label for="<?php echo $data->id; ?>-newsletter-email">E-mail</label>
-  <input type="email" id="<?php echo $data->id; ?>-newsletter-email">
-</form>
