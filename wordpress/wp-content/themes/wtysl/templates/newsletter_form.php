53a3066570031619bc96a240f73d9ac25744ae62
Fred Bergman <fred@fbergman.se>
2016-04-15 19:26:23 +0200

    Update newsletter signup design

--- wordpress/wp-content/themes/wtysl/templates/newsletter_form.php
+++ wordpress/wp-content/themes/wtysl/templates/newsletter_form.php
@@ -1,14 +1,16 @@
-<form class="Newsletter" action="//whattookyousolong.<?php echo MAILCHIMP_DATACENTER; ?>.list-manage.com/subscribe/post?u=<?php echo MAILCHIMP_USER; ?>&amp;id=<?php echo MAILCHIMP_ID; ?>" method="post" target="_blank" novalidate>
-  <p>Subscribe to our newsletter</p>
+<form class="Newsletter" action="//whattookyousolong.<?php echo MAILCHIMP_DATACENTER; ?>.list-manage.com/subscribe/post?u=<?php echo MAILCHIMP_USER; ?>&amp;id=<?php echo MAILCHIMP_ID; ?>" method="post">
+  <h3 class="Headline Headline--3">Subscribe to our newsletter</h3>
 
-  <label class="Newsletter-label" for="newsletter-email">Email:</label>
+  <div class="Newsletter-inputWrapper">
+    <label class="u-visuallyhidden" for="newsletter-email">Email:</label>
 
-  <input class="Newsletter-input" id="newsletter-email" type="email" name="EMAIL">
+    <input class="Newsletter-input" id="newsletter-email" type="email" name="EMAIL" placeholder="E-mail" required="required">
 
-  <?php # real people should not fill this in and expect good things - do not remove this or risk form bot signups ?>
-  <div class="u-visuallyhidden">
-    <input class="u-visuallyhidden" type="text" name="b_<?php echo MAILCHIMP_USER; ?>_<?php echo MAILCHIMP_ID; ?>" tabindex="-1" value="">
+    <?php # real people should not fill this in and expect good things - do not remove this or risk form bot signups ?>
+    <div class="u-visuallyhidden">
+      <input class="u-visuallyhidden" type="text" name="b_<?php echo MAILCHIMP_USER; ?>_<?php echo MAILCHIMP_ID; ?>" tabindex="-1" value="">
+    </div>
+
+    <input class="Newsletter-submit" type="submit" value="→" name="subscribe">
   </div>
-
-  <input class="Newsletter-submit" type="submit" value="Subscribe" name="subscribe">
 </form>
